cf2c1792a9cc2fb07b412d5c4fdf2f985866cdd2
sumnic <sum_ith@mail.ru>
2024-10-26 21:50:29 +0300

    correction of a grammatical error

--- server/src/messages/messages.service.ts
+++ server/src/messages/messages.service.ts
@@ -53,7 +53,7 @@ export class MessagesService {
                 params.append('access_token', DATA.access_token);
                 params.append('peer_ids', `${peer_ids.join(',')}`);
                 params.append('random_id', '0');
-                params.append('message', `Отправитель: ${user.first_name} ${user.last_name} \nСообщение: ${message.message} \nПерейти в сообщениям: ${DATA.client_url}/messages/${dto.location}`);
+                params.append('message', `Отправитель: ${user.first_name} ${user.last_name} \nСообщение: ${message.message} \nПерейти к сообщениям: ${DATA.client_url}/messages/${dto.location}`);
 
                 const response = await fetch('https://api.vk.com/method/messages.send', {
                     method: 'POST',
