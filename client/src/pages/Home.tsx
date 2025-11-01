06a32af3bf41efd78a4d087543b6ae82513ffc61
SumNic <sum_ith@mail.ru>
2025-11-01 12:58:34 +0300

    test vk auth

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import HeaderLogoMobile from '../components/HeaderLogo/HeaderLogoMobile';
 import HeaderLogoRegistr from '../components/HeaderLogo/HeaderLogoRegistr';
 import NavMiddle from '../components/Nav_middle/NavMiddle';
 import { observer } from 'mobx-react-lite';
-import { FOUNDERS_ROUTE, HOME_ROUTE, VK_ID_APP } from '../utils/consts';
+import { FOUNDERS_ROUTE, HOME_ROUTE } from '../utils/consts';
 import { useStoreContext } from '../contexts/StoreContext';
 import { useEffect } from 'react';
 import { App } from '@capacitor/app';
